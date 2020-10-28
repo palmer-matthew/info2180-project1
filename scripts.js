63e456826a9bf37ab8d7f6fad8dd09f908f73d38
Matthew Palmer <palmer.matthew4567@gmail.com>
2020-10-28 16:30:04 -0500

    Progress on Email Validation

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
 /* Add your JavaScript to this file */
+"uses strict";
+
+window.addEventListener("load", (event) => {
+    let formuse = document.querySelector("form");
+    formuse.addEventListener("submit", (e) => {
+        e.preventDefault();
+        let mailformat = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/; //provides the regex expression for a email-address
+        let data = document.forms[0]["email"].value;
+        let message = document.querySelector(".message");
+        if(data.match(mailformat)){
+            console.log("True");
+            message.textContent = `Thank you! Your email address ${data} was added to our mailing list`;
+        }else{
+            console.log("False");
+            message.textContent = "Please enter a valid email address.";
+        }
+        document.querySelector("form").reset();
+    });
+});
